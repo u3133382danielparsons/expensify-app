fe870f381dc1ad404e371027ccefb620b6608046
Daniel Parsons <danielparsons3133382@gmail.com>
2018-10-01 02:37:19 +1000

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -16,6 +16,7 @@ export const startAddExpense = (expenseData = {}) => {
             createdAt = 0
             } = expenseData;
             const expense = { description, note, amount, createdAt };
+
             return database.ref('expenses').push(expense).then((ref) => {
                 dispatch(addExpense({
                     id: ref.key,
@@ -30,12 +31,29 @@ export const removeExpense = ( { id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
+
+export const startRemoveExpense = ({ id} = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    }
+}
 
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
